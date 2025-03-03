0f47c246d94b319320f216e0e99332e5eaae919b
Bohdan <bogdankruk29@gmail.com>
2025-03-03 17:24:02 +0200

    Add hamburger menu functionality and update header and navigation structure

--- script.js
+++ script.js
@@ -1,15 +1,61 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Завантажуємо хедер
-    fetch("Parts/header.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("header-placeholder").innerHTML = data;
-        });
+document.addEventListener("DOMContentLoaded", function () {
+  // Завантажуємо хедер
+  fetch("Parts/header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("header-placeholder").innerHTML = data;
+    });
 
-    // Завантажуємо навігацію
-    fetch("Parts/nav.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("nav-placeholder").innerHTML = data;
-        });
+  // Завантажуємо навігацію
+  fetch("Parts/nav.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("nav-placeholder").innerHTML = data;
+    });
+});
+
+
+
+function showMenu() {
+  let nav = document.querySelector("nav");
+  let placeholder = document.querySelector("#nav-placeholder");
+
+  if (nav.classList.contains("open")) {
+    // Приховати меню
+    nav.classList.remove("open");
+    placeholder.classList.remove("open");
+  } else {
+    // Показати меню
+    nav.classList.add("open");
+    placeholder.classList.add("open");
+  }
+}
+
+window.addEventListener("resize", function() {
+  let nav = document.querySelector("nav");
+  let placeholder = document.querySelector("#nav-placeholder");
+
+  if (window.innerWidth > 1000) {
+    // Якщо ширина екрану більша за 1000px, то меню повинно залишатися відкритим
+    if (nav.classList.contains("open")) {
+      nav.classList.add("open");
+      placeholder.classList.add("open");
+    }
+  } else {
+    // Якщо ширина екрану менша за 1000px, закриваємо меню
+    nav.classList.remove("open");
+    placeholder.classList.remove("open");
+  }
+});
+
+// Закриття меню при кліку поза ним
+document.addEventListener("click", function(event) {
+  let nav = document.querySelector("nav");
+  let placeholder = document.querySelector("#nav-placeholder");
+
+  // Перевіряємо, чи клік був поза меню або кнопкою для його відкриття
+  if (!nav.contains(event.target) && !placeholder.contains(event.target)) {
+    nav.classList.remove("open");
+    placeholder.classList.remove("open");
+  }
 });
