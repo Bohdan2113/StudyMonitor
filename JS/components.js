86fbc6c84304e653f438a37399b6239427597634
Bohdan <bogdankruk29@gmail.com>
2025-03-23 18:37:55 +0200

    Add PWA support with service worker and manifest file

--- JS/components.js
+++ JS/components.js
@@ -1,3 +1,12 @@
+window.onload = function () {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(() => console.log("Service Worker registered"))
+      .catch((err) => console.error("Service Worker registration failed", err));
+  }
+};
+
 document.addEventListener("DOMContentLoaded", async () => {
   try {
     await includeHTML("/Components/header.html", "header-placeholder");
